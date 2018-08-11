ae2e8e0dbae85ca62faf94a3e9a70140119158aa
Nicci Sheets <niccisheets@aol.com>
2018-08-11 19:46:38 -0400

    failed test for removing unwanted dashes, spaces, and ensuring if contains x that it is the last character of the string

--- isbn_checker.rb
+++ isbn_checker.rb
@@ -1,3 +1,24 @@
 def isbn10_ready(isbn)
 	""
+	# isbn2 = isbn.gsub(/[^0-9xX]/,'').gsub(/x/,'X')
+	# if isbn2.include? "X" 
+	# 	if true && isbn2[-1] == "X"
+	# 		isbn2
+	# 	else 
+	# 		nil
+	# 	end
+	# else
+	# 	isbn2
+	# end
 end
+
+
+
+
+
+#isbn = "0471958697"
+#isbn = "0-321-14653-0"
+#isbn = "877195869x"
+#isbn = "877195x869"
+
+#p isbn10_ready(isbn)
--- test_isbn_checker.rb
+++ test_isbn_checker.rb
@@ -14,10 +14,16 @@ class TestIsbnChecker < Minitest::Test
 		assert_equal(String, isbn10_ready(isbn).class)
 	end
 
-	# def test_assert_removes_characters_except_valid_numbers_or_xX
-	# 	isbn = "0471958697"
-	# 	assert_equal()
-
+	def test_assert_removes_characters_except_valid_numbers_or_xX
+		isbn = "0471958697"
+		assert_equal("0471958697", isbn10_ready(isbn))
+		isbn = "0-321-14653-0"
+		assert_equal("0321146530", isbn10_ready(isbn))
+		isbn = "877195869x"
+		assert_equal("877195869X", isbn10_ready(isbn))
+		isbn = "877195x869"
+		assert_equal(nil, isbn10_ready(isbn))
+	end
 
 
 
