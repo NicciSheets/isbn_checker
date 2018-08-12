c9a3f7251b3d1f4687eeb9202c7819e3e091dc8e
Nicci Sheets <niccisheets@aol.com>
2018-08-12 01:36:48 -0400

    fixed some bugs for internal characters that are not integers

--- isbn_checker.rb
+++ isbn_checker.rb
@@ -1,27 +1,27 @@
-
-
-def isbn_ready(isbn)
+def isbn10_ready(isbn)
 	""
 	isbn2 = isbn.gsub(/[^0-9xX]/,'').gsub(/x/,'X')
-	if isbn2.include? "X" 
-		if true && isbn2.length == 10 && isbn2[-1] == "X"
-			isbn2
-		else 
-			"Invalid ISBN"
-		end
+	if isbn2.length != 10
+		"Invalid ISBN"
 	else
-		isbn2
+		if isbn2.include? "X" 
+			if true && isbn2[-1] == "X"
+				isbn2
+			elsif true && isbn2[1] != "X"
+				"Invalid ISBN"
+			else
+				"Invalid ISBN"
+			end
+		else
+			isbn2
+		end
 	end
 end
-# 	if isbn2.length != 10
-# 		nil
-# 	else 
-# 		isbn2
-# 	end
-# end
+
+
 
 def isbn10_checksum(isbn)
-	isbn2 = isbn_ready(isbn)
+	isbn2 = isbn10_ready(isbn)
 	if isbn2 == "Invalid ISBN"
 		"Invalid ISBN"
 	else
@@ -54,13 +54,28 @@ def isbn10_validation(isbn)
 end
 
 
-
+def isbn13_ready(isbn)
+	""
+	isbn2 = isbn.gsub(/[^0-9]/,'')
+	if isbn2.length == 13
+		isbn2
+	else
+		"Invalid ISBN"
+	end
+end
 
 #isbn = "0471958697"
 #isbn = "0-321-14653-0"
 #isbn = "877195869x"
 #isbn = "877195x869"
+#isbn = "877195z869"
+
+#p isbn.gsub(/[^0-9xX]/, '')
 
 #isbn_ready(isbn)
 #p isbn10_checksum(isbn)
 #p isbn10_validation(isbn)
+
+#isbn = "978047005902x"
+#isbn = "97804x7148648"
+#p isbn.gsub(/[^0-9]/,'')
--- test_isbn_checker.rb
+++ test_isbn_checker.rb
@@ -5,63 +5,24 @@ class TestIsbnChecker < Minitest::Test
 
 	def test_assert_removes_chars_except_valid_numbers_or_xX_and_returns_nil_unless_x_last_char_isbn10
 		isbn = "0471958697"
-		assert_equal("0471958697", isbn_ready(isbn))
+		assert_equal("0471958697", isbn10_ready(isbn))
 		isbn = "0-321-14653-0"
-		assert_equal("0321146530", isbn_ready(isbn))
+		assert_equal("0321146530", isbn10_ready(isbn))
 		isbn = "877195869x"
-		assert_equal("877195869X", isbn_ready(isbn))
+		assert_equal("877195869X", isbn10_ready(isbn))
 		isbn = "877195x869"
-		assert_equal("Invalid ISBN", isbn_ready(isbn))
+		assert_equal("Invalid ISBN", isbn10_ready(isbn))
 	end
 
 	def test_assert_returns_string_class
 		isbn = "0471958697"
-		assert_equal(String, isbn_ready(isbn).class)
+		assert_equal(String, isbn10_ready(isbn).class)
 		isbn = "0-321-14653-0"
-		assert_equal(String, isbn_ready(isbn).class)
+		assert_equal(String, isbn10_ready(isbn).class)
 		isbn = "877195869x"
-		assert_equal(String, isbn_ready(isbn).class)
+		assert_equal(String, isbn10_ready(isbn).class)
 	end
-
-	# def test_assert_isbn10_is_10_characters_else_returns_nil
-	# 	isbn = "0471958697"
-	# 	assert_equal("0471958697", isbn10_ready(isbn))
-	# 	isbn = "0-321-14653-0"
-	# 	assert_equal("0321146530", isbn10_ready(isbn))
-	# 	isbn = "877195869x"
-	# 	assert_equal("877195869X", isbn10_ready(isbn))
-	# 	isbn = "471958697"
-	# 	assert_nil(nil, isbn10_ready(isbn))
-	# 	isbn = "0-21-14653-0"
-	# 	assert_nil(nil, isbn10_ready(isbn))
-	# 	isbn = "77195869x"
-	# 	assert_nil(nil, isbn10_ready(isbn))
-	# 	isbn = "77195x869"
-	# 	assert_nil(nil, isbn10_ready(isbn))
-	# end
-
-	def test_assert_removes_chars_except_valid_numbers_for_isbn13
-		isbn = "9780470059029"
-		assert_equal("9780470059029", isbn_ready(isbn))
-		isbn = "978-0-13-149505-0"
-		assert_equal("9780131495050", isbn_ready(isbn))
-		isbn = "978 0 471 48648 0"
-		assert_equal("9780471486480", isbn_ready(isbn))
-		isbn = "97804x71486480"
-		assert_equal("Invalid ISBN", isbn_ready(isbn))
-		isbn = "978047005902x"
-		assert_equal("Invalid ISBN", isbn_ready(isbn))
-	end
-
-	def test_assert_returns_string_class
-		isbn = "9780470059029"
-		assert_equal(String, isbn_ready(isbn).class)
-		isbn = "978-0-13-149505-0"
-		assert_equal(String, isbn_ready(isbn).class)
-		isbn = "978 0 471 48648 0"
-		assert_equal(String, isbn_ready(isbn).class)
-	end
-
+	
 	def test_assert_returns_checksum_isbn10
 		isbn = "0471958697"
 		assert_equal(7, isbn10_checksum(isbn))
@@ -79,7 +40,47 @@ class TestIsbnChecker < Minitest::Test
 		isbn = "877195869x"
 		assert_equal("Valid ISBN10", isbn10_validation(isbn))	
 		isbn = "877195x869"
+		assert_equal("Invalid ISBN", isbn10_validation(isbn))
+		isbn = "877195a869"
 		assert_equal("Invalid ISBN", isbn10_validation(isbn))		
 	end	
 
+	def test_assert_removes_chars_except_valid_numbers_for_isbn13
+		isbn = "9780470059029"
+		assert_equal("9780470059029", isbn13_ready(isbn))
+		isbn = "978-0-13-149505-0"
+		assert_equal("9780131495050", isbn13_ready(isbn))
+		isbn = "978 0 471 48648 0"
+		assert_equal("9780471486480", isbn13_ready(isbn))
+		isbn = "97804x7148648"
+		assert_equal("Invalid ISBN", isbn13_ready(isbn))
+		isbn = "978047005902x"
+		assert_equal("Invalid ISBN", isbn13_ready(isbn))
+	end
+
+	def test_assert_ISBN13_returns_string_class
+		isbn = "9780470059029"
+		assert_equal(String, isbn13_ready(isbn).class)
+		isbn = "978-0-13-149505-0"
+		assert_equal(String, isbn13_ready(isbn).class)
+		isbn = "978 0 471 48648 0"
+		assert_equal(String, isbn13_ready(isbn).class)
+	end
+
 end
+# def test_assert_isbn10_is_10_characters_else_returns_nil
+	# 	isbn = "0471958697"
+	# 	assert_equal("0471958697", isbn10_ready(isbn))
+	# 	isbn = "0-321-14653-0"
+	# 	assert_equal("0321146530", isbn10_ready(isbn))
+	# 	isbn = "877195869x"
+	# 	assert_equal("877195869X", isbn10_ready(isbn))
+	# 	isbn = "471958697"
+	# 	assert_nil(nil, isbn10_ready(isbn))
+	# 	isbn = "0-21-14653-0"
+	# 	assert_nil(nil, isbn10_ready(isbn))
+	# 	isbn = "77195869x"
+	# 	assert_nil(nil, isbn10_ready(isbn))
+	# 	isbn = "77195x869"
+	# 	assert_nil(nil, isbn10_ready(isbn))
+	# end
