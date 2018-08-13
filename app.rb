bc22216d936644bf30c2bd4f79614ce847961eac
Nicci Sheets <niccisheets@aol.com>
2018-08-13 11:57:31 -0400

    fixed app.rb so that the hash allows for isbn and validity params to output

--- app.rb
+++ app.rb
@@ -4,13 +4,19 @@ enable :sessions
 
 get '/' do
 	puts params
-	validity = validity 
-	erb :checker, locals:{validity: validity}
+	final_answer = final_answer
+	isbn_orig = isbn_orig
+	validity_final = validity_final
+	erb :checker, locals:{final_answer: final_answer, isbn_orig: isbn_orig, validity_final: validity_final}
 end
 
 post '/isbn_checker' do
 	puts params
-	validity = which_one(params[:isbn])
-	p "validity is #{validity}"
-	erb :checker, locals:{validity: validity}
+	final_answer = isbn_hash(params[:isbn])
+	p "final answer is #{final_answer}"
+	isbn_orig = final_answer.values[0]
+	p "isbn_orig is #{isbn_orig}"
+	validity_final = final_answer.values[1]
+	p "validity_final is #{validity_final}"
+	erb :checker, locals:{final_answer: final_answer, isbn_orig: isbn_orig, validity_final: validity_final}
 end
