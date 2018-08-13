cfddedf8f4e9730245b958aef21b5afdf5f098d3
Nicci Sheets <niccisheets@aol.com>
2018-08-12 20:49:37 -0400

    working on dynamic data to a modal

--- app.rb
+++ app.rb
@@ -3,5 +3,12 @@ require_relative 'isbn_checker.rb'
 enable :sessions
 
 get '/' do
-  ''
+	erb :checker
+end
+
+post '/isbn_checker' do
+	puts params
+	validity = which_one(params[:isbn])
+	p "validity is #{validity}"
+	erb :checker, locals:{validity: validity}
 end
