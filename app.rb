f70cd3104d8b99f942e8a8481560029e43177060
Nicci Sheets <niccisheets@aol.com>
2018-08-14 01:46:17 -0400

    got all results for isbn and valid or not to print out as results FINALLY

--- app.rb
+++ app.rb
@@ -4,31 +4,29 @@ enable :sessions
 
 get '/' do
 	final_answer = final_answer
-	validity_final = validity_final
-	isbn_orig = isbn_orig
-	erb :checker, locals:{final_answer: final_answer, isbn_orig: isbn_orig, validity_final: validity_final}
+	isbn = isbn
+	# validity_final = validity_final
+	# isbn_orig = isbn_orig
+	erb :checker, locals:{final_answer: final_answer, isbn: isbn}
 end
 
 post '/isbn_checker' do
 	puts params
-	isbn = params[:isbn] 
-	isbn2 = isbn.split
-	p "#{isbn2} and #{isbn2.class} and #{isbn2.length}"
-	final_answer = {}
-	isbn2.each do
-		for isbn in isbn2
-			final_answer["Validity"] = which_one(isbn)
-		end
-		final_answer["Validity"]
+	isbn = params[:isbn_check]
+	# isbn_arr = []
+	# isbn = isbn.split('\r\n')
+	p "#{isbn} and #{isbn.class} and #{isbn.length}"
+	final_answer = []
+	isbn.split("\r\n").each do
+		p isbn.split("\r\n")
+		final_answer << which_one(isbn)
 	end
-	isbn2.each do
-		for isbn in isbn2
-			final_answer["ISBN"] = isbn
-		end
-		final_answer["ISBN"]
-	end
-	isbn_orig = final_answer.values
-	validity_final = final_answer.values
+	p final_answer
+	erb :checker, locals:{final_answer: final_answer, isbn: isbn}
+end
+	# final_answer["#{isbn}" => "#{validity}"]
+	# isbn_orig = final_answer.values
+	# validity_final = final_answer.values
 
 	# # final_answer = isbn_hash(isbn)
 	# #p "final answer is #{final_answer}"
@@ -36,5 +34,4 @@ post '/isbn_checker' do
 	# p "isbn_orig is #{isbn_orig} and its class is #{isbn_orig.class}"
 	# validity_final = final_answer.values[1]
 	# p "validity_final is #{validity_final}"
-	erb :checker, locals:{final_answer: final_answer, isbn_orig: isbn_orig, validity_final: validity_final}
-end
+	
