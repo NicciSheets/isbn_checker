b3a4d729217ca986c711b2a89cd30f1feda139a1
Nicci Sheets <niccisheets@aol.com>
2018-08-15 14:42:53 -0400

    got results from textarea to show in a table at end

--- app.rb
+++ app.rb
@@ -3,20 +3,22 @@ require_relative 'isbn_checker.rb'
 enable :sessions
 
 get '/' do
-	final_answer = final_answer
-	isbn = isbn
-	erb :checker, locals:{final_answer: final_answer, isbn: isbn}
+	final_answer = [] 
+	isbn = []
+	big_array = big_array
+	erb :checker, locals:{final_answer: [""], isbn: [""], big_array: [""]}
 end
 
 post '/isbn_checker' do
-	puts params
 	isbn = params[:isbn_check].split("\r\n")
-	p "#{isbn} and #{isbn.class} and #{isbn.length}"
 	final_answer = []
 	isbn.each do |isbn|
 		final_answer << which_one(isbn)
 	end
 	final_answer
-	erb :checker, locals:{final_answer: final_answer, isbn: isbn}
+	a = isbn
+	b = final_answer
+	big_array = a.zip(b)
+	erb :checker, locals:{final_answer: final_answer, isbn: isbn, big_array: big_array}
 end
 	
