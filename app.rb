ba85ddf816a0ce065f0f4c6c4de7b6a7517d8520
Nicci Sheets <niccisheets@aol.com>
2018-08-18 10:17:25 -0400

    fixed issue with leading and trailing whitespace

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require 'aws-sdk'
+require 'csv'
+require 'json'
 require_relative 'isbn_checker.rb'
+require_relative 'csv_checker.rb'
 enable :sessions
 
 get '/' do
@@ -10,7 +14,8 @@ get '/' do
 end
 
 post '/isbn_checker' do
-	isbn = params[:isbn_check].split("\r\n")
+	isbn = params[:isbn_check].split("\r\n").map(&:strip)
+	p"isbn params are #{params[:isbn_check]} and isbn is #{isbn}"
 	final_answer = []
 	isbn.each do |isbn|
 		final_answer << which_one(isbn)
