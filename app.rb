c9d1554181cdac529fc7431d4f25f283e3658edd
Nicci Sheets <niccisheets@aol.com>
2018-08-20 16:27:55 -0400

    got the bucket connected

--- app.rb
+++ app.rb
@@ -5,16 +5,18 @@ require 'json'
 require_relative 'isbn_checker_refactor.rb'
 enable :sessions
 
-# load 'local_ENV.rb' if File.exist?('local_ENV.rb')
+load 'local_ENV.rb' if File.exist?('local_ENV.rb')
 
-# s3 = Aws::S3::Client.new(profile: 'm.m.default.2018', region: 'us-east-2')
+s3 = Aws::S3::Client.new(profile: 'm.m.default.2018', region: 'us-east-2')
 
-# s3.put_object(bucket: 'sand.castle', body: validated, key: "valid_check#{count}.csv")
+s3.put_object(bucket: 'sand.castle', body: validated, key: "csv_file.csv")
 
 
 get '/' do
+	validated = validated
+	isbn = isbn
 	big_array = big_array
-	erb :checker, locals:{big_array: [""]}
+	erb :checker, locals:{big_array: [""], validated: [""], isbn: [""]}
 end
 
 post '/isbn_checker' do
@@ -28,6 +30,6 @@ post '/isbn_checker' do
 	a = isbn
 	b = final_answer
 	big_array = a.zip(b)
-	erb :checker, locals:{big_array: big_array}
+	erb :checker, locals:{big_array: big_array, validated: validated, isbn: isbn}
 end
 	
