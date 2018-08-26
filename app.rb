0ceb909bef4c0bc8110698a149be99096db22149
Nicci Sheets <niccisheets@aol.com>
2018-08-26 02:38:47 -0400

    added commented out csv input for textarea testing

--- app.rb
+++ app.rb
@@ -1,16 +1,16 @@
 require 'sinatra'
 require 'aws-sdk'
-# require 'csv'
+require 'csv'
 require 'json'
 require_relative 'isbn_checker_refactor.rb'
-# require_relative 'local_env.rb'
+require_relative 'local_env.rb'
 enable :sessions
 
-# load 'local_env.rb' if File.exist?('local_env.rb')
+load 'local_env.rb' if File.exist?('local_env.rb')
 
-# s3 = Aws::S3::Client.new(profile: 'NicciSheets', region: 'us-east-2')
-
-# s3.put_object(bucket: 'isbn-bucket81', key: "csv_file.csv")
+s3 = Aws::S3::Client.new(:access_key_id => ENV["S3_KEY"],
+ :secret_access_key => ENV["S3_SECRET"]
+  )
 
 
 get '/' do
@@ -34,3 +34,18 @@ post '/isbn_checker' do
 	erb :checker, locals:{big_array: big_array, final_answer: final_answer, isbn: isbn}
 end
 	
+
+
+	# *** Below are sample ISBN numbers to use in textarea, the first 6 are valid and the last 6 are invalid
+# 0471958697
+# 0-321-14653-0
+# 877195869x
+# 9780470059029
+# 978-0-13-149505-0
+# 978 0 471 48648 0
+# -
+# 877195x869
+# 877195a869
+# 97804x7148648
+# 9780470059022
+# 97804a0059029
